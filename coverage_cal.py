4d6ac4e07fe85ca3964b5330943bba0f32f6c8b7
Malavika Vasist <malavika.vasist47@gmail.com>
2021-05-03 17:01:43 +0200

    coverage changes

--- coverage_cal.py
+++ coverage_cal.py
@@ -12,6 +12,7 @@ import math
 import pandas as pd
 import os
 import sys
+import pickle
 
 from torch.distributions import Independent, Distribution
 from fab_priors import BoxUniform_New
@@ -171,28 +172,46 @@ TT = torch.cat(list_of_tensors_T)[:,1:]
 
 inference = inference.append_simulations(TT, XX) #TT[:1000], XX[:1000]) #(TT, XX) 
 
-# Training the density estimator
+#Training the density estimator
     
 density_estimator = inference.train()
 
-# i/p and o/p - 10k simulations
+#Saving the density_estimator for later use
+# an_obj = inference()
+# file_to_store = open("density_estimators/inference_3param_100kSim_mlp_e.pickle", "wb")
+# pickle.dump(an_obj, file_to_store)
+# file_to_store.close()
 
-for k in range(1, 101):
-    if k==1: continue
-    if k==15: continue
-    dfX_M= pd.read_csv('/home/mvasist/simulations/3_params/1/X_10ksim_TintkIRLg'+ str(k) + '.csv', low_memory = False) #chunksize=1000, iterator=True, dtype={'col2': np.float32}
-    dfT_M= pd.read_csv('/home/mvasist/simulations/3_params/1/T_10ksim_TintkIRLg'+ str(k) + '.csv', low_memory = False)
-    X.append(dfX_M.values) #for x,t in zip(dfX_M,dfT_M):
-    T.append(dfT_M.values)
+an_obj1 = density_estimator
+file_to_store1 = open("density_estimators/density_estimator_3param_100kSim_mlp_e.pickle", "wb")
+pickle.dump(an_obj1, file_to_store1)
+file_to_store1.close()
+
+
+# # Using a trained density estimator
+# filehandler = open('density_estimator/density_estimator_4param_1MSim_mlp_116e.pickle', 'r') 
+# density_estimator = pickle.load(filehandler)
+
+# i/p and o/p - 10k simulations
+X_=[]
+T_=[]
+
+for k_ in range(1, 101):
+    if k_==1: continue
+    if k_==15: continue
+    dfX_M_= pd.read_csv('/home/mvasist/simulations/3_params/1/X_10ksim_TintkIRLg'+ str(k_) + '.csv', low_memory = False) #chunksize=1000, iterator=True, dtype={'col2': np.float32}
+    dfT_M_= pd.read_csv('/home/mvasist/simulations/3_params/1/T_10ksim_TintkIRLg'+ str(k_) + '.csv', low_memory = False)
+    X_.append(dfX_M_.values) #for x,t in zip(dfX_M,dfT_M):
+    T_.append(dfT_M_.values)
     
-comb_np_array_X = np.vstack(X)
-comb_np_array_T = np.vstack(T)
-Xframe = pd.DataFrame(comb_np_array_X)
-Tframe = pd.DataFrame(comb_np_array_T)
-list_of_tensors_X = [torch.tensor(np.array(Xframe),dtype=torch.float32)]
-list_of_tensors_T = [torch.tensor(np.array(Tframe),dtype=torch.float32)] #torch.tensor
-outputs = torch.cat(list_of_tensors_X)[:, 1:]
-inputs = torch.cat(list_of_tensors_T)[:,1:]
+comb_np_array_X_ = np.vstack(X_)
+comb_np_array_T_ = np.vstack(T_)
+Xframe_ = pd.DataFrame(comb_np_array_X_)
+Tframe_ = pd.DataFrame(comb_np_array_T_)
+lit_of_tensors_X_ = [torch.tensor(np.array(Xframe_),dtype=torch.float32)]
+list_of_tensors_T_ = [torch.tensor(np.array(Tframe_),dtype=torch.float32)] #torch.tensor
+outputs = torch.cat(list_of_tensors_X_)[:, 1:]
+inputs = torch.cat(list_of_tensors_T_)[:,1:]
 
 # n = 100
 # indices = np.random.choice(np.arange(len(inputs)), n, replace=False)
@@ -220,6 +239,6 @@ df_samples = pd.DataFrame(cov_cl, columns=['method', 'cov'])
 df_samples.to_csv('/home/mvasist/scripts/coverage/coverage_cl.csv',mode='a', header=False)
    
 
-    
+   
     
  
